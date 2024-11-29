58041ed03da6906d322183d7dea3d778fcae4976
Henrique Kist Closs <henriquekcloss@gmail.com>
2024-11-29 22:12:43 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const connectionsActive = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const connectionsActiveValue = connectionsActive.rows[0].count;
+
+  const postgresVersion = await database.query("SHOW server_version;");
+  const postgresVersionValue = postgresVersion.rows[0].server_version;
+
   response.status(200).json({
-    chave: "valor",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: connectionsActiveValue,
+        version: postgresVersionValue,
+      },
+    },
   });
 }
 
